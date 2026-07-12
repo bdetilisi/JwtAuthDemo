d1f1f7017edb3540e50b2cb03c55eab4932b3574
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2026-07-12 20:06:22 +0200

    feat: add base Login endpoint to AuthController

--- src/JwtAuthDemo.WebApi/Controllers/AuthController.cs
+++ src/JwtAuthDemo.WebApi/Controllers/AuthController.cs
@@ -9,20 +9,38 @@ namespace JwtAuthDemo.WebApi.Controllers;
 [ApiController]
 public class AuthController : ControllerBase
 {
-    private static UserEntity _user = new()
+    private static readonly UserEntity _userEntity = new()
     {
         Email = string.Empty,
         PasswordHashed = string.Empty
     };
 
+    private static readonly PasswordHasher<UserEntity> _passwordHasher = new();
+
     [HttpPost("register")]
     public ActionResult<UserEntity> Register(UserDto request)
     {
-        var passwordHasher = new PasswordHasher<UserEntity>();
+        _userEntity.Email = request.Email;
+        _userEntity.PasswordHashed = _passwordHasher.HashPassword(_userEntity, request.Password);
 
-        _user.Email = request.Email;
-        _user.PasswordHashed = passwordHasher.HashPassword(_user, request.Password);
+        return Ok(_userEntity);
+    }
 
-        return Ok(_user);
+    [HttpPost("login")]
+    public ActionResult<string> Login(UserDto request)
+    {
+        if(_userEntity.Email != request.Email)
+        {
+            return Unauthorized("User not found.");
+        }
+
+        var result = _passwordHasher.VerifyHashedPassword(_userEntity, _userEntity.PasswordHashed, request.Password);
+        if(result == PasswordVerificationResult.Failed)
+        {
+            return Unauthorized("Either username or password is incorrect");
+        }
+
+        string token = "success";
+        return Ok(token);
     }
 }
