e8adf8d00e1dce46b26745a9fa186e76793fffb0
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2026-07-12 21:03:33 +0200

    feat: add App db context

---
+++ src/JwtAuthDemo.WebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using JwtAuthDemo.WebApi.Data.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthDemo.WebApi.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<UserEntity> Users { get; set; }
+}
