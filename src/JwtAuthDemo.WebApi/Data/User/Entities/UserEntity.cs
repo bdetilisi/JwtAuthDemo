24d292d6e90e184da3149b1e30af1e616ce418aa
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2026-07-15 20:21:18 +0200

    feat: add Refresh token properties to UserEntity + Migrationos

--- src/JwtAuthDemo.WebApi/Data/User/Entities/UserEntity.cs
+++ src/JwtAuthDemo.WebApi/Data/User/Entities/UserEntity.cs
@@ -6,4 +6,6 @@ public class UserEntity
     public required string Email { get; set; }
     public required string PasswordHashed { get; set; }
     public required string Role { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
 }
